4994f4be06bfc0313664720f3fcf7a752e30ccb7
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2025-12-17 19:59:00 -0600

    fix: update build scripts in package.json for deployment options and remove base path from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Prometheus/', // use '/' if deploying to Cloudflare Pages
 })
